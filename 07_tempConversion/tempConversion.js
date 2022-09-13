b49be8a4e381e39dd363b1365020c0b77fbb69f6
lu <umeanlikethat@protonmail.com>
2022-09-13 14:48:10 +0200

    finished exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num1) {
+  return result1 = parseFloat(((num1 - 32) * 5/9).toFixed(1));
+  
 };
 
-const ctof = function() {
+const ctof = function(num2) {
+  
+  return result1 = parseFloat((((num2 * (9/5)) + 32).toFixed(1)));
 
 };
 
